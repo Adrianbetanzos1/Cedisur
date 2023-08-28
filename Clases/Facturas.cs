f8499317c128f21656d2ab799fe9d6d3f5b99623
Adrian <86131461+Adrianbetanzos1@users.noreply.github.com>
2023-08-28 12:15:56 -0600

    Add files via upload
    trabajo al 90%

---
+++ Clases/Facturas.cs
@@ -0,0 +1,81 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Cedisur.Clases
+{
+    public class Facturas
+    {
+        public int ID_facturas { get; set; }
+        public string FacturaN { get; set; }
+
+        //Datos de provisiones
+
+        public DateTime fechaFactura { get; set; }
+        public int diasVencimiento { get; set; }
+        public int importe { get; set; }
+        public int abono { get; set; }
+        public int saldoMXP { get; set; }
+
+       
+
+        //Datos Totales
+
+        public int saldoPendiente { get; set; }
+        public int provisionesAportadas { get; set; }
+
+
+        public DataTable MostrarFactura()
+        {
+
+            using SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
+            SqlDataAdapter da = new SqlDataAdapter("DT_MostrarFactura", conexion);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexion.Close();
+            return dt;
+        }
+
+        public static DataTable Mostrar(string args)
+        {
+            using SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
+            conexion.Open();
+            string consulta = "select * from Factura where FacturaN like '%" + args + "%'";
+            SqlDataAdapter da = new SqlDataAdapter(consulta, conexion);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexion.Close();
+            return dt;
+            
+        }
+
+
+        public static DataTable MostrarID(string args)
+        {
+            using SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
+            string consulta = "select * from Factura where ID_factura like '%" + args + "%'";
+            SqlDataAdapter da = new SqlDataAdapter(consulta, conexion);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexion.Close();
+            return dt;
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- Clases/Proveedor.cs
+++ Clases/Proveedor.cs
@@ -1,80 +1,47 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 
 namespace Cedisur.Clases
 {
     public class Proveedor
     {
         public int ID_proveedor { get; set; }
-        public string nombreProveedor { get; set; }
+        public string? NombreProveedor { get; set; }
+        public DateTime FechaRegistro { get; set; }
 
-        //Datos de provisiones
-        public int FacturaN { get; set; }
-        public DateTime fechaFactura { get; set; }
-        public int diasVencimiento { get; set; }
-        public int importe { get; set; }
-        public int abono { get; set; }
-        public int saldoMXP { get; set; }
 
-       
-
-        //Datos Totales
-
-        public int saldoPendiente { get; set; }
-        public int provisionesAportadas { get; set; }
-
+        
 
         public DataTable MostrarProveedores()
         {
 
-            using SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
-            SqlDataAdapter da = new SqlDataAdapter("DT_MostrarProveedores", conexion);
+            using SqlConnection conexion = new("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
+            SqlDataAdapter da = new ("DT_MostrarProveedor", conexion);
             da.SelectCommand.CommandType = CommandType.StoredProcedure;
-            DataTable dt = new DataTable();
+            DataTable dt = new();
             da.Fill(dt);
             conexion.Close();
             return dt;
         }
 
-        public static DataTable Mostrar(string args)
+
+        public static DataTable MostrarNombreProveedores(string args)
         {
-            using SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
-            conexion.Open();
-            string consulta = "select * from Proveedores where nombreProveedor like '%" + args + "%'";
-            SqlDataAdapter da = new SqlDataAdapter(consulta, conexion);
 
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            conexion.Close();
-            return dt;
-            
-        }
+            using SqlConnection conexion = new ("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
+            string consulta = "select * from Proveedor where nombreProveedor like '%" + args + "%'";
+            SqlDataAdapter da = new (consulta, conexion);
 
-
-        public static DataTable MostrarID(string args)
-        {
-            using SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
-            string consulta = "select * from Proveedores where ID_proveedor like '%" + args + "%'";
-            SqlDataAdapter da = new SqlDataAdapter(consulta, conexion);
-
-            DataTable dt = new DataTable();
+            DataTable dt = new();
             da.Fill(dt);
             conexion.Close();
             return dt;
         }
-
-
-
-
-
-
 
 
     }
