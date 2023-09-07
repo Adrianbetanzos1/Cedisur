8cd1001433ef794b5488345f7acf3ecc69a1dbde
Adrian <86131461+Adrianbetanzos1@users.noreply.github.com>
2023-09-07 08:50:49 -0600

    Commit nuevo total

--- Clases/Facturas.cs
+++ Clases/Facturas.cs
@@ -1,19 +1,19 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace Cedisur.Clases
+namespace CedisurB.Clases
 {
-    public class Facturas
+    class Facturas
     {
+
         public int ID_facturas { get; set; }
-        public string? FacturaN { get; set; }
+        public string FacturaN { get; set; }
 
         //Datos de provisiones
 
@@ -23,7 +23,7 @@ namespace Cedisur.Clases
         public int Abono { get; set; }
         public int SaldoMXP { get; set; }
 
-       
+
 
         //Datos Totales
 
@@ -34,48 +34,52 @@ namespace Cedisur.Clases
         public static DataTable MostrarFactura()
         {
 
-            using SqlConnection conexion = new ("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
-            SqlDataAdapter da = new ("DT_MostrarFacturasActualizada", conexion);
-            da.SelectCommand.CommandType = CommandType.StoredProcedure;
-            DataTable dt = new();
-            da.Fill(dt);
-            conexion.Close();
-            return dt;
+            using (SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true"))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("DT_MostrarFacturasActualizada", conexion);
+                
+                DataTable dt = new DataTable();
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.Fill(dt);
+                conexion.Close();
+                return dt;
+            }
+                
         }
 
         public static DataTable Mostrar(string args)
         {
-            using SqlConnection conexion = new("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
-            conexion.Open();
-            string consulta = "select * from Factura where FacturaN like '%" + args + "%'";
-            SqlDataAdapter da = new(consulta, conexion);
-
-            DataTable dt = new();
-            da.Fill(dt);
-            conexion.Close();
-            return dt;
+            using (SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true"))
+            {
+                conexion.Open();
+                string consulta = "select * from Factura where FacturaN like '%" + args + "%'";
+                SqlDataAdapter da = new SqlDataAdapter(consulta, conexion);
+                
+                DataTable dt = new DataTable();
+                
+                da.Fill(dt);
+                conexion.Close();
+                return dt;
+            }
             
+
         }
 
 
         public static DataTable MostrarIDProveedor(string args)
         {
-            using SqlConnection conexion = new ("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true");
-            string consulta = "select * from Factura where ID_proveedor like '%" + args + "%'";
-            SqlDataAdapter da = new(consulta, conexion);
-
-            DataTable dt = new();
-            da.Fill(dt);
-            conexion.Close();
-            return dt;
+            using (SqlConnection conexion = new SqlConnection("Server=DESKTOP-717JV41\\SQLEXPRESS; Database=Cedisur;  integrated security= true"))
+            {
+                string consulta = "select * from Factura where ID_proveedor like '%" + args + "%'";
+                SqlDataAdapter da = new SqlDataAdapter(consulta, conexion);
+                
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                conexion.Close();
+                return dt;
+            }
+                
         }
-
-
-
-
-
-
-
 
     }
 }
